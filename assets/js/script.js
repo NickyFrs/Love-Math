a4c2aabbc648ebf763eb6ef3ba3c259ba2cc8aaa
Nicky Frias <nickyfrs@gmail.com>
2022-02-27 17:21:10 +0000

    event listener added to load page before executing.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,24 @@
+
+//Code to wait for the html page to load before any other code be executed
+
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons){
+        button.addEventListener("click", function () {
+            if(this.getAttribute("data-type") === "submit"){
+                alert("You clicked Submit!");
+            }
+            else {
+                let gameType = this.getAttribute("data-type");
+                alert(`you clicked ${gameType}`);
+            }
+        })
+    }
+})
+
+//
+
 function runGame() {
 
 }
